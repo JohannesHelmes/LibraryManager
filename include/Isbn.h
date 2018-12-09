446e2f51376b817bd1bd68d267e93fe478904e6c
Johannes Helmes <helmes@thp.uni-koeln.de>
2018-12-09 21:07:40 +0100

    autocomplete wip

--- include/Isbn.h
+++ include/Isbn.h
@@ -16,15 +16,17 @@ public:
     char getTyp() const { return typ_; };
 
     bool checkValid() const;
-    std::list< std::string > autoComplete() const;
+    std::list< std::string > autoComplete( int32_t size );
 private:
     void updateTyp();
-    int8_t calcChecksum() const;
+    int8_t calcChecksum( std::vector<int8_t> const& number ) const;
+    std::string num2strg( std::vector<int8_t> const& number ) const;
 
 
     std::vector<int8_t> num_;
     char typ_; // 'U' - unknown, 'X' - Isbn10, 'E' - EAN ( Isbn13 ), 'W' - wrong
-    static const int32_t _maxAutoComplete = 100;
+    int32_t autoCompCount_;
+    static const int32_t _maxAutoComp = 100;
     const std::set<char> _allowedDigits {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'X' };
 };
 
--- src/Isbn.cpp
+++ src/Isbn.cpp
@@ -6,6 +6,7 @@ Isbn::Isbn()
 {
     num_.clear();
     typ_ = 'U';
+    autoCompCount_ = 0;
 }
 
 void Isbn::pushDigit( char digit )
@@ -33,10 +34,7 @@ std::string Isbn::getNumber() const
 {
     if ( typ_ == 'W' )
         return "";
-    std::string numStr = "";
-    for ( auto itrDig = num_.rbegin(); itrDig != num_.rend(); ++itrDig )
-        numStr.push_back( *itrDig == 10 ? 'X' : char(int8_t('0') + *itrDig ));
-    return numStr;
+    return num2strg( num_ );
 }
 
 void Isbn::updateTyp()
@@ -54,30 +52,38 @@ void Isbn::updateTyp()
 bool Isbn::checkValid() const
 {
     if ( typ_ == 'X' && num_.size() == 10 )
-        return calcChecksum() == num_[9];
+        return calcChecksum( num_ ) == num_[9];
     if ( typ_ == 'E' && num_.size() == 13 )
-        return calcChecksum() == num_[12];
+        return calcChecksum( num_ ) == num_[12];
     return false;
 }
 
-int8_t Isbn::calcChecksum() const
+std::string Isbn::num2strg( std::vector<int8_t> const& number ) const
 {
-    if ( typ_ == 'X' && num_.size() >= 9 )
+    std::string numStr = "";
+    for ( auto itrDig = number.rbegin(); itrDig != number.rend(); ++itrDig )
+        numStr.push_back( *itrDig == 10 ? 'X' : char(int8_t('0') + *itrDig ));
+    return numStr;
+}
+
+int8_t Isbn::calcChecksum( std::vector<int8_t> const& number ) const
+{
+    if ( typ_ == 'X' && number.size() >= 9 )
     {
         int8_t sum = 0;
         for ( int i = 0; i <= 8; ++i )
         {
-            sum += num_[i] * ( i + 1 );
+            sum += number[i] * ( i + 1 );
             sum %= 11;
         }
         return sum;
     }
-    if ( typ_ == 'E' && num_.size() >= 12 )
+    if ( typ_ == 'E' && number.size() >= 12 )
     {
         int8_t sum = 0;
         for ( int i = 0; i <= 11; i+=2 )
         {
-            sum += ( num_[i] + 3 * num_[i+1] );
+            sum += ( number[i] + 3 * number[i+1] );
             sum %= 10;
         }
         sum = ( 10 - sum ) % 10 ;
@@ -86,6 +92,39 @@ int8_t Isbn::calcChecksum() const
     return -1; // error code
 }
 
-std::list< std::string > Isbn::autoComplete() const
+std::list< std::string > Isbn::autoComplete( int32_t size )
 {
+    std::list< std::string > autoCompList;
+    if ( checkValid() )
+    {
+        autoCompList.push_back( num2strg( num_ ) );
+        return autoCompList;
+    }
+
+    size = std::min( size, _maxAutoComp );
+    std::vector<int8_t> autoComp( num_ );
+    int32_t cnt = 0;
+    int8_t crtDgt = num_.size();
+    if ( typ_ == 'X' )
+    {
+        autoComp.resize( 9, 0 );
+        while ( cnt < size && crtDgt < 9 )
+        {
+            autoComp[ 9 ] = calcChecksum( autoComp );
+            autoCompList.push_back( num2strg( autoComp ) );
+            ++autoComp[ crtDgt ];
+            while ( autoComp[ crtDgt ] == 10 )
+            {
+                autoComp[ crtDgt ] = 0;
+                ++autoComp[ ++crtDgt ];
+            }
+            ++cnt;
+        }
+    }
+    else if ( typ_ == 'E' )
+    {
+    }
+
+    autoCompCount_ += autoCompList.size();
+    return autoCompList;
 }
